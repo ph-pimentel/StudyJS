4e3a8a005d62a79a9c688cd7d16228f44107d562
ph-pimentel <pedropimentel17henrique@gmail.com>
2025-07-03 16:37:15 -0300

    Revisão de alguns conceitos básicos de JavaScript

---
+++ 01-TiposDadosEOperadores/TipoDeDados.js
@@ -0,0 +1,43 @@
+// Tipos de Dados em JavaScript
+
+//1- Number (Aceitando todo dado número, com casas decimais, negativos)
+// let number = 2;
+// let number2 = 23.40;
+// let number3 = -10.00;
+// console.log(typeof number3);
+
+// 2- Operações Aritméticas (Sendo os mesmo de outras linguagens (+-*/%))
+// const number = 5;
+// const number2 = 10;
+// console.log(number2 % number);
+
+// 3- Strings (Tipo texto, sendo primitivo no JS)
+// const Name = 'Peter Parker';
+// console.log(Name);
+
+// 4- Concatenaçao (Forma de "juntar" strings mesmo estando separadas, ou usado em variavéis)
+// const Name = 'Peter Parker';
+// const Job = 'Photographer and Spider-man in lezy times'
+// console.log(Name + Job)
+
+// 5- Template Literals (Forma mais correta de misturar texto e variavéis ou códigos JS)
+// const Name = 'Kakarot';
+// const Name2 = 'Vegeta';
+// console.log(`Nome do protagonista: ${Name} Nome Rival: ${Name2}`);
+
+// 6- Booleans (Possuindo dois valores: true and false, necessitando de condições)
+// const number1 = 10;
+// const number2 = 7000;
+// console.log(number1 > number2)
+// Condições ou Comparações ( > e < ; >= e <=; == (igual) ; != (diferente) ; === (idêntico) ; !== diferenças)
+
+// 7- Operadores Lógicos (&& (AND) ; || (OR) ; !(NOT));
+// Tabela Verdade = && as condições devem ser true ; 
+// Tabela Verdade = || pelo menos uma deve ser true;
+// Tabela Verdade = ! Inverte o valor lógico;
+// const number = 10;
+// const number2 = 15;
+// console.log(number < number2  && number2 > number);
+// console.log(number < number2  || number2 !== number);
+
+// 8- Empty Value (null = valor atribuído para expressar 'nada' e undefined = valor não definido )
