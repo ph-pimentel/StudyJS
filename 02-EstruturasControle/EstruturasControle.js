7070a30e88881099c3ec31fe00e0c38db336b357
ph-pimentel <pedropimentel17henrique@gmail.com>
2025-07-13 12:31:06 -0300

    Organização dos arquivos, adicionando resumos de cada tópico

--- 02-EstruturasControle/EstruturasControle.js
+++ 02-EstruturasControle/EstruturasControle.js
@@ -20,11 +20,9 @@
 // console.log(Math.floor(money)) // Arredonda para baixo números decimais
 
 
-// -> Estruturas de Controle
+/// -> Estruturas de Controle
 
 // 1- IF; ELSE; ELSE IF
-// IF = Funciona com base em uma condição se for true executa a caixa de código, se for false retorna o ELSE;
-// IF ELSE = Funciona como uma segunda caixa de retorno, adicionando outro retorno com outra condição
 // const Name = prompt("What's your name?");
 // if (Name !== null && Name.trim() !== ""){
 //     alert(`Your name is ${Name}`);
@@ -34,46 +32,7 @@
 // }
 // * Método Trim = Remove espaços do início ao fim
 
-
-// -> Estruturas de Repetição (Repete blocos de código até a condição imposta for false)
-
-// 1- While (Imprime o código dentro do bloco até atingir a condição, verificando antes de executar)
-// let i = 0;
-// while (i <= 10){
-//     console.log(i)
-//     i++
-// }
-
-// 2- Do While (Faz uma execução primeiro depois verifica a condição)
-// let i = 0;
-// do {
-//     console.log(`Valor do i antes da incrementação ${i}`);
-//     i++
-// } while (i <= 5);
-
-// 3- For (Declaramos a incrementação, condição, progressão do loop)
-// for (let i = 0;i <= 5;i++){
-//     console.log(`Aqui está o valor de i em for: ${i}`);
-//     //Mais simples 
-// }
-
-// 4 - break (encerra o loop com uma determinada condição ou não)
-// for (let i = 0; i <= 100; i++){
-//     console.log(`Value of i: ${i}`);
-//     if (i >= 15){ //Através de uma condição ele repete 15 vezes
-//         break
-//     }
-// }
-
-// 5 - continue (continua o loop após uma certa condição)
-// for (let i = 0; i <= 20; i++){
-//     if (i % 2 === 0){
-//         console.log(`Value números pares: ${i}`)
-//         continue // Pula números que não derem true na condição
-//     }
-// }
-
-// 6 - Switch (forma mais limpa ao invés de utilizar if/else muitas vezes)
+// 2 - Switch
 // let money = 2000.00;
 // switch (true) { 
 //     case (money >= 5000.00): //Adicionado um parâmetro para filtrar quem pode comprar
@@ -82,4 +41,46 @@
 //     default:
 //         console.log('Não consegue comprar PS5');
 // }
+
+
+/// -> Estruturas de Repetição
+
+// 1- While
+// let i = 0;
+// while (i <= 10){
+//     console.log(i)
+//     i++
+// }
+
+// 2- Do While
+// let i = 0;
+// do {
+//     console.log(`Valor do i antes da incrementação ${i}`);
+//     i++
+// } while (i <= 5);
+
+// 3- For
+// for (let i = 0;i <= 5;i++){
+//     console.log(`Aqui está o valor de i em for: ${i}`);
+//     //Mais simples 
+// }
+
+/// -> Estruturas de controle de desvio
+
+// 1 - Break
+// for (let i = 0; i <= 100; i++){
+//     console.log(`Value of i: ${i}`);
+//     if (i >= 15){ //Através de uma condição ele repete 15 vezes
+//         break
+//     }
+// }
+
+// 2 - Continue
+// for (let i = 0; i <= 20; i++){
+//     if (i % 2 === 0){
+//         console.log(`Value números pares: ${i}`)
+//         continue // Pula números que não derem true na condição
+//     }
+// }
+
 
